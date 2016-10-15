2ed8261b4ac3dd86dc825790d12668268c42e742
gazoon <g89250741413@gmail.com>
2016-10-15 23:59:58 +0300

    add local_settings.py

--- meetrack/settings.py
+++ meetrack/settings.py
@@ -176,3 +176,7 @@ REDIS = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STORAGE_URL = 'http://localhost:8000' + MEDIA_URL
+try:
+    from .local_settings import *
+except ImportError:
+    pass
