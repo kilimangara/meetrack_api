248645ca057c21ac352c73a5e635c0c68ee1ceaa
gazoon <g89250741413@gmail.com>
2016-11-16 18:18:35 +0300

    start auth changing

--- authtoken/authentication.py
+++ authtoken/authentication.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import exceptions
+from rest_framework.views import exception_handler
 from rest_framework.authentication import TokenAuthentication
 
 from . import tokens
@@ -10,9 +11,11 @@ class RedisTokenAuthentication(TokenAuthentication):
     User = get_user_model()
 
     def authenticate_credentials(self, key):
+        a = 10 / 0
         try:
             user_id = tokens.authenticate(key)
             user = self.User.objects.get(pk=user_id)
         except (tokens.AuthenticationFailed, self.User.DoesNotExist):
-            raise exceptions.AuthenticationFailed(_('Invalid token.'))
+            raise exceptions.AuthenticationFailed(
+                {'error': {'type': '2222', 'status_code': 401, 'description': 'ballbbb'}})
         return user, key
